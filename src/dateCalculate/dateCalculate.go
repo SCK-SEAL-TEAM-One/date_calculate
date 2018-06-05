aa220cfabd59bbb6b9a454078b27480f374d341d
IRONMAN <ironman@IRONMANs-Mac-mini.local>
2018-06-05 15:40:12 +0700

    [Edited] ฟังก์ชัน Diff สามารถทำงานได้ตามเงื่อนไขการทดสอบ, แก้ไขไฟล์ให้อยู่ถูก package จาก src/date_calculate.go เป็น src/dateCalculate/dateCalculate.go

---
+++ src/dateCalculate/dateCalculate.go
@@ -0,0 +1,18 @@
+package dateCalculate
+
+import (
+	"time"
+)
+
+func Diff(startDate time.Time, endDate time.Time) int {
+	secondsInDays := (60 * 60 * 24)
+	if startDate.After(endDate) {
+		startDate, endDate = endDate, startDate
+	}
+	startTimestamp := startDate.Unix()
+	endTimestamp := endDate.Add(time.Hour * 24).Unix()
+
+	durationUnix := endTimestamp - startTimestamp
+	durationDays := int(durationUnix) / secondsInDays
+	return durationDays
+}
