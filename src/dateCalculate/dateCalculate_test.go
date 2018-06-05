ae4e3e3dc5a4fd64459cef8de532f9772b29cd77
IRONMAN <ironman@IRONMANs-Mac-mini.local>
2018-06-05 15:27:14 +0700

    [Edited] ย้ายไฟล์ dateCalculate.go จาก src เป็น src/dateCalculate/dateCalculate.go

---
+++ src/dateCalculate/dateCalculate_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_Diff_Input_4_1_2018_4_6_2018_should_be_152(t *testing.T) {
+	startDate := time.Date(2018, 1, 4, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2018, 6, 4, 0, 0, 0, 0, time.UTC)
+
+	expectedResult := 152
+	duration := Diff(startDate, endDate)
+	if expectedResult != duration {
+		t.Error("Expected: ", expectedResult, " but got ", duration)
+	}
+}
