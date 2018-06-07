f660eb1209580f2d6eae0b910fe1d38d9c7c29b3
Deadpool <deadpool@Deadpools-Mac-mini.local>
2018-06-07 11:50:30 +0700

    [Edited] ย้ายฟังก์ชัน dorationHandler ไปที่ไฟล์ httpHandler.go ใน package dateCalculate

---
+++ src/dateCalculate/httpHandler.go
@@ -0,0 +1,25 @@
+package dateCalculate
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func DurationHandler(w http.ResponseWriter, r *http.Request) {
+	durationResponse := map[string]string{
+		"from":        "Thursday, 4 January 2018",
+		"to":          "Monday, 4 June 2018",
+		"days":        "152 days",
+		"years":       "5 months, 1 day",
+		"seconds":     "13,132,800 seconds",
+		"minutes":     "218,880 minutes",
+		"hours":       "3,648 hours",
+		"weeks":       "21 weeks and 5 days",
+		"ratioOfYear": "41.64% of 2018",
+	}
+
+	err := json.NewEncoder(w).Encode(durationResponse)
+	if err != nil {
+		panic(err)
+	}
+}
--- src/main/main.go
+++ src/main/main.go
@@ -1,33 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	"dateCalculate"
 	"log"
 	"net/http"
 )
 
-func durationHandler(w http.ResponseWriter, r *http.Request) {
-	durationResponse := map[string]string{
-		"from":        "Thursday, 4 January 2018",
-		"to":          "Monday, 4 June 2018",
-		"days":        "152 days",
-		"years":       "5 months, 1 day",
-		"seconds":     "13,132,800 seconds",
-		"minutes":     "218,880 minutes",
-		"hours":       "3,648 hours",
-		"weeks":       "21 weeks and 5 days",
-		"ratioOfYear": "41.64% of 2018",
-	}
-
-	err := json.NewEncoder(w).Encode(durationResponse)
-	if err != nil {
-		panic(err)
-	}
-
-}
 func main() {
-	http.HandleFunc("/duration", durationHandler)
+	http.HandleFunc("/duration", dateCalculate.DurationHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
